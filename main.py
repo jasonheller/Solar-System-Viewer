2dd2bb20d3847ebed0b503f65f2ef8ddc258efdb
Jason Heller <79803619+jasonheller@users.noreply.github.com>
2021-06-04 00:28:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from ursina import * 
+# Code by Me
+from ursina import *
 
 root = Ursina()
-window.fps_counter.enabled = False
 window.exit_button.visible = False
+window.fps_counter.enabled = False
 window.borderless = False
 window.show_ursina_splash = True
 window.fullscreen = False
-
+camera.orthographic = False
+space = load_texture('./assets/space.png')
+Sky(texture=space)
 ec = EditorCamera(rotation_smoothing=2, enabled=True, rotation=(0,0,0), scale=2)
 
 title = Text(text='Solar System Viewer', color=color.white, position=(-0.1,0.49,0))
@@ -23,28 +26,28 @@ class SolarViewExit(Button):
         )
     def on_click(self):
         deleteEntities()
-        sve.visible = False
-        moon.visible = True
-        uranus.visible = True
-        neptune.visible = True
-        ceres.visible = True
-        saturn.visible = True
-        jupiter.visible = True
-        venus.visible = True
-        mercury.visible = True
-        mars.visible = True
-        earth.visible = True 
-        earth_button.visible = True
-        mars_button.visible = True
-        mercury_button.visible = True
-        venus_button.visible = True
-        ceres_button.visible = True
-        jupiter_button.visible = True
-        saturn_button.visible = True
-        neptune_button.visible = True
-        uranus_button.visible = True
-        moon_button.visible = True
-        sun_button.visible = True
+        sve.enabled = False
+        moon.enabled = True
+        uranus.enabled = True
+        neptune.enabled = True
+        ceres.enabled = True
+        saturn.enabled = True
+        jupiter.enabled = True
+        venus.enabled = True
+        mercury.enabled = True
+        mars.enabled = True
+        earth.enabled = True
+        earth_button.enabled = True
+        mars_button.enabled = True
+        mercury_button.enabled = True
+        venus_button.enabled = True
+        ceres_button.enabled = True
+        jupiter_button.enabled = True
+        saturn_button.enabled = True
+        neptune_button.enabled = True
+        uranus_button.enabled = True
+        moon_button.enabled = True
+        sun_button.enabled = True
 
 class SolarView(Button):
     def __init__(self):
@@ -58,17 +61,17 @@ class SolarView(Button):
     def on_click(self):
         deleteEntities()
         changeToSolarView()
-        sve.visible = True
-        pluto2.visible = True
-        uranus2.visible = True
-        neptune2.visible = True
-        saturn2.visible = True
-        jupiter2.visible = True
-        venus2.visible = True
-        mercury2.visible = True
-        mars2.visible = True
-        earth2.visible = True
-        sun2.visible = True
+        sve.enabled = True
+        pluto2.enabled = True
+        uranus2.enabled = True
+        neptune2.enabled = True
+        saturn2.enabled = True
+        jupiter2.enabled = True
+        venus2.enabled = True
+        mercury2.enabled = True
+        mars2.enabled = True
+        earth2.enabled = True
+        sun2.enabled = True
 
 from solar_animation import *
 from planets import *
